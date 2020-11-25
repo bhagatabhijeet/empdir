d7bfde22fad56aa73cf2e67dd73069e1dce5e368
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-25 14:15:40 -0800

    remove clutter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h4>Edit <code>src/App.js</code> and save to reload.</h4>
         </p>
         <a
           className="App-link"
