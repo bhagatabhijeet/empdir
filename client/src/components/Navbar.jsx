82b45b8a19a72d833e35fb1be22cc68c6ed4b43a
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-03 00:45:40 -0800

    cleanse

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,25 +4,16 @@ import {
   ThemeProvider,
 } from "@material-ui/core/styles";
 import {AppBar,Toolbar,Typography} from "@material-ui/core";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
-// import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => (  
   {
   root: {
     flexGrow: 1,
     color: theme.palette.primary.contrastText,
-    backgroundColor:theme.palette.background.default
-    
+    backgroundColor:theme.palette.background.default    
   },
   menuButton: {
-    marginRight: theme.spacing(2),
-    // color: theme.status.danger,
-    // backgroundColor:theme.bg.backgroundColor,
-    // color:theme.bg.backgroundColor,
+    marginRight: theme.spacing(2),    
   },
   title: {
     flexGrow: 1,
@@ -32,44 +23,23 @@ const useStyles = makeStyles((theme) => (
 
 function Navbar() {
   const classes = useStyles();
-  // const theme = useTheme();
-  // console.log(theme);
-  // console.log(classes);
-  // useEffect(() => {
-  //   theme= state.checked?theme1:theme2;
-  //   console.log(theme.palette.primary.main);
-  // }, [state])
-
   
-
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          
-
           <Typography variant="h6" className={classes.title}>
             AbhiCorp Global Employee Directory
           </Typography>
-
-          
         </Toolbar>
       </AppBar>
     </div>
   );
 }
-// const theme = createMuiTheme({
-//   status: {
-//     danger: red[500],
-//   },
-//   palette:{
-//     primary:{main:red[300]}
-//   }
-// });
+
 export default function CustomNavbar(props) {
   return (
-    <ThemeProvider theme={props.theme}>
-      {/* {console.log(props.theme.palette.primary.main)} */}
+    <ThemeProvider theme={props.theme}>      
       <Navbar />
     </ThemeProvider>
   );
