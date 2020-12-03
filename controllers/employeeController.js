8e4a1af706df1097c0aca2306195c17fe3e4a6b7
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-03 00:32:16 -0800

    change projection for employee

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -3,7 +3,10 @@ const db = require("../model");
 module.exports = {
   getNonSensitiveData: async (req, res) => {
     try {
-      const result = await db.Employee.find({});
+      const result = await db.Employee.find(
+        {},
+        "-login.password -login.salt -login.md5 -login.sha1 -login.sha256"
+      ).exec();
       res.json(result);
     } catch (e) {
       console.log("User Controller", e);
--- model/Employee.js
+++ model/Employee.js
@@ -7,54 +7,54 @@ const EmployeeSchema = new Schema({
     default:'male',
     trim:true,
   },
-  // name:{
-  //   title:String,
-  //   first:{type:String,required:[true,'first name is required'],trim:true},
-  //   last:{type:String,required:[true,'last name is required'],trim:true},
-  // },
-  // location:{
-  //   street:{
-  //     number:{type:Number},
-  //     name:{type:String}
-  //   },
-  //   city:String,
-  //   state:String,
-  //   country:String,
-  //   postcode:String,
-  //   coordinates:{
-  //     latitude:Number,
-  //     longitude:Number
-  //   },
-  //   timezone:{
-  //     offset:String,
-  //     description:String
-  //   }
-  // },
-  // email : {type:String,required:[true,'email is required'],trim:true},
-  // login : {
-  //     uuid : String,
-  //     username : {type:String,required:[true,'username is required'],trim:true},
-  //     password : {type:String,required:[true,'password is required'],trim:true},
-  //     salt : String,
-  //     md5 : String,
-  //     sha1 : String,
-  //     sha256 :String
-  // },
-  // dob : {
-  //     date : {type:String,required:[true,'dob.date is required'],trim:true},
-  //     age : String
-  // },
-  // registered : {
-  //     date : String,
-  //     age : String
-  // },
-  // phone :  String,
-  // cell :  String,
-  // picture : {
-  //     large :String,
-  //     medium :String,
-  //     thumbnail : String
-  // },
+  name:{
+    title:String,
+    first:{type:String,required:[true,'first name is required'],trim:true},
+    last:{type:String,required:[true,'last name is required'],trim:true},
+  },
+  location:{
+    street:{
+      number:{type:Number},
+      name:{type:String}
+    },
+    city:String,
+    state:String,
+    country:String,
+    postcode:String,
+    coordinates:{
+      latitude:Number,
+      longitude:Number
+    },
+    timezone:{
+      offset:String,
+      description:String
+    }
+  },
+  email : {type:String,required:[true,'email is required'],trim:true},
+  login : {
+      uuid : String,
+      username : {type:String,required:[true,'username is required'],trim:true},
+      password : {type:String,required:[true,'password is required'],trim:true},
+      salt : String,
+      md5 : String,
+      sha1 : String,
+      sha256 :String
+  },
+  dob : {
+      date : {type:String,required:[true,'dob.date is required'],trim:true},
+      age : String
+  },
+  registered : {
+      date : String,
+      age : String
+  },
+  phone :  String,
+  cell :  String,
+  picture : {
+      large :String,
+      medium :String,
+      thumbnail : String
+  },
   nat :String
  });
 
