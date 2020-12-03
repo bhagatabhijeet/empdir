add6d8c62f70adc42d44fe08b1c080aa2407d187
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-03 01:02:30 -0800

    cleanse and document

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { Typography, Grid } from "@material-ui/core";
-import Divider from "@material-ui/core/Divider";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
+import { Typography, Grid, Divider, Box, Link } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
-// import chypeTransLogo from "../assets/images/chypeShortLogo-trans.png";
-import { makeStyles,ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   divider: {
-    // Theme Color, or use css color in quote
     backgroundColor: "#dff3fc",
   },
-  footerGrid:{
-    backgroundColor:theme.palette.primary.main,
+  footerGrid: {
+    backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
-    marginTop:'auto'
-  }
+    marginTop: "auto",
+  },
 }));
 
 const Footer = () => {
@@ -25,11 +20,6 @@ const Footer = () => {
     <Grid
       container
       direction="row"
-      // style={{
-      //   minHeight: "20vh",
-      //   backgroundColor: "#020006",
-      //   color: "#d0effc",
-      // }}
       justify="space-evenly"
       alignItems="center"
       className={classes.footerGrid}
@@ -53,18 +43,21 @@ const Footer = () => {
 
       <Box lg={4} textAlign="center">
         <Box mb={3}>
-        <Typography variant="caption" display="block">
+          <Typography variant="caption" display="block">
             About
           </Typography>
           <Typography variant="caption">
-           EmpDir is simple Employee search Application developed using React, Mongo, Express
+            EmpDir is simple Employee search Application developed using React,
+            Mongo, Express
           </Typography>
         </Box>
-        <Divider/>
+        <Divider />
         <Typography variant="caption">
-          Data Courtesy : <Link href='https://randomuser.me/'  color="inherit"
-              target="_blank">randomuser.me/</Link>
-          </Typography>
+          Data Courtesy :{" "}
+          <Link href="https://randomuser.me/" color="inherit" target="_blank">
+            randomuser.me/
+          </Link>
+        </Typography>
         <Box mb={3}>
           <Typography variant="caption">
             &copy; EmpDir - {new Date().getFullYear()}
@@ -96,11 +89,11 @@ const Footer = () => {
   );
 };
 
+// Wrap Footer in ThemeProvider
 export default function CustomFooter(props) {
   return (
     <ThemeProvider theme={props.theme}>
-      {/* {console.log(props.theme.palette.primary.main)} */}
-      <Footer {...props}/>
+      <Footer {...props} />
     </ThemeProvider>
   );
-  }
+}
