1e10f485be947d8c621013e5ec302182f79c4bfc
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-02 17:14:40 -0800

    resolve build error

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ mongoose.connect(process.env.MONGODB_URI, {
 mongoose.set('debug', true);
 
 const PORT=process.env.PORT||3001;
+const app=express();
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/",express.static("client/build"));
+  app.use(express.static("client/build"));
 }
-const app=express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
